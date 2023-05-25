88a466c536f179ada6d61e9c55e7104a54bf1d77
Nahoon Kang <ejrtks102020@gmail.com>
2023-05-25 11:28:29 +0000

    feat: add validation

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from argparse import ArgumentParser
 
 import torch
 from torch import cuda
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from torch.optim import lr_scheduler
 from tqdm import tqdm
 
@@ -36,72 +36,78 @@ def parse_args():
     seed_everything(args.seed)
 
     if args.input_size % 32 != 0:
-        raise ValueError('`input_size` must be a multiple of 32')
+        raise ValueError("`input_size` must be a multiple of 32")
 
     return args
 
+
 def do_training(
-                data_dir,
-                model_dir,
-                device,
-                image_size,
-                input_size,
-                num_workers,
-                batch_size,
-                learning_rate,
-                max_epoch,
-                save_interval,
-                ignore_tags,
-                augmentation,
-                seed,
-                extractor_pth,
-                enable_amp,
-                project_name,
-                patience,                
-                ):    
+    data_dir,
+    model_dir,
+    device,
+    image_size,
+    input_size,
+    num_workers,
+    batch_size,
+    learning_rate,
+    max_epoch,
+    save_interval,
+    ignore_tags,
+    seed,
+    extractor_pth,
+    enable_amp,
+    project_name,
+    val_ratio,
+    val_batch_size
+):
     dataset = SceneTextDataset(
         data_dir,
-        split='train',
+        split="train",
         image_size=image_size,
         crop_size=input_size,
         ignore_tags=ignore_tags,
-        augmentation=augmentation,
+        color_jitter=False,
     )
     dataset = EASTDataset(dataset)
-    num_batches = math.ceil(len(dataset) / batch_size)
+    train_size = int(len(dataset) * (1 - val_ratio))
+    valid_size = int(len(dataset) * val_ratio)
+    trainset, validset = random_split(dataset, [train_size, valid_size])
+
+    train_num_batches = math.ceil(len(trainset) / batch_size)
     train_loader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=num_workers
+        trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers
     )
+    if val_ratio > 0:
+        valid_num_batches = math.ceil(len(validset) / val_batch_size)
+        valid_loader = DataLoader(
+                validset, batch_size=val_batch_size, shuffle=True, num_workers=num_workers
+            )    
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = EAST(extractor_pth=extractor_pth)
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[max_epoch // 2], gamma=0.1)
-    
+    scheduler = lr_scheduler.MultiStepLR(
+        optimizer, milestones=[max_epoch // 2], gamma=0.1
+    )
+
     if enable_amp:
         scaler = torch.cuda.amp.GradScaler()
-        
-    # Early Stop
-    best_mean_loss = float('inf')
-    patience_limit = args.patience
-    patience = 0
-        
+
     model.train()
     for epoch in range(max_epoch):
         epoch_loss, epoch_start = 0, time.time()
-        with tqdm(total=num_batches) as pbar:
+        with tqdm(total=train_num_batches) as pbar:
             for img, gt_score_map, gt_geo_map, roi_mask in train_loader:
-                pbar.set_description('[Epoch {}]'.format(epoch + 1))
+                pbar.set_description("[Epoch {}]".format(epoch + 1))
 
-                optimizer.zero_grad()                
+                optimizer.zero_grad()
                 if enable_amp:
                     # Mixed Precision으로 Operation들을 casting
                     with torch.cuda.amp.autocast(enable_amp):
-                        loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
+                        loss, extra_info = model.train_step(
+                            img, gt_score_map, gt_geo_map, roi_mask
+                        )
 
                     # Loss를 scaling한 후에 backward진행
                     scaler.scale(loss).backward()
@@ -110,7 +116,9 @@ def do_training(
                     # 다음 iter를 위한 scale update
                     scaler.update()
                 else:
-                    loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
+                    loss, extra_info = model.train_step(
+                        img, gt_score_map, gt_geo_map, roi_mask
+                    )
                     loss.backward()
                     optimizer.step()
 
@@ -119,18 +127,21 @@ def do_training(
 
                 pbar.update(1)
                 val_dict = {
-                    'Cls loss': extra_info['cls_loss'], 'Angle loss': extra_info['angle_loss'],
-                    'IoU loss': extra_info['iou_loss']
+                    "Cls loss": extra_info["cls_loss"],
+                    "Angle loss": extra_info["angle_loss"],
+                    "IoU loss": extra_info["iou_loss"],
                 }
                 pbar.set_postfix(val_dict)
 
-                wandb.log({
-                    'train/dice_score': 1 - extra_info['cls_loss'],
-                    'train/epoch_loss': epoch_loss,
-                    'train/cls_loss': extra_info['cls_loss'],
-                    'train/angle_loss': extra_info['angle_loss'],
-                    'train/iou_loss': extra_info['iou_loss'],
-                    })
+                wandb.log(
+                    {
+                        "train/dice_score": 1 - extra_info["cls_loss"],
+                        "train/epoch_loss": epoch_loss,
+                        "train/cls_loss": extra_info["cls_loss"],
+                        "train/angle_loss": extra_info["angle_loss"],
+                        "train/iou_loss": extra_info["iou_loss"],
+                    }
+                )
 
         scheduler.step()
 
@@ -147,32 +158,19 @@ def do_training(
             os.makedirs(model_dir, exist_ok=True)
             ckpt_fpath = osp.join(model_dir, 'latest.pth')
             torch.save(model.state_dict(), ckpt_fpath)
-        
-        # Early Stop
-        if best_mean_loss > mean_loss:
-            best_mean_loss = mean_loss
-            patience = 0
-            
-            os.makedirs(model_dir, exist_ok=True)
-            ckpt_fpath = osp.join(model_dir, 'best.pth')
-            torch.save(model.state_dict(), ckpt_fpath)
-        else:
-            patience += 1
-            if patience >= patience_limit:
-                break
-    
-    print(f"Best Mean Loss : {best_mean_loss}")
+
 
 def main(args):
     wandb.init(
         entity="level2-hiboostcamp-2",
         project="data-centric",
         name=args.project_name,
-        config=args
+        config=args,
     )
-    
+
     do_training(**args.__dict__)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = parse_args()
     main(args)
